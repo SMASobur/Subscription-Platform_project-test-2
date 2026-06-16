bf99b60b2398b7c7b3e822306536d9f32a77e4b7
SMASobur <sikdar.se@gmail.com>
2026-06-16 14:54:09 +0200

    Fix OperatorMapper
    Ignore Auditing & Auto-generated fields

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/mapper/OperatorMapper.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/mapper/OperatorMapper.java
@@ -1,6 +1,7 @@
 package se.lexicon.subscriptionapi.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import se.lexicon.subscriptionapi.domain.entity.Operator;
 import se.lexicon.subscriptionapi.dto.request.OperatorRequest;
 import se.lexicon.subscriptionapi.dto.response.OperatorResponse;
@@ -8,6 +9,10 @@ import se.lexicon.subscriptionapi.dto.response.OperatorResponse;
 @Mapper(componentModel = "spring")
 public interface OperatorMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "plans", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Operator toEntity(OperatorRequest request);
 
     OperatorResponse toResponse(Operator operator);
