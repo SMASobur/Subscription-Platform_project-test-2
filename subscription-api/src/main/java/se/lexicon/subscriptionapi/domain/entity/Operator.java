444c256924a829e50d7656863d22044293446929
SMASobur <sikdar.se@gmail.com>
2026-06-16 10:28:42 +0200

    Update Operator Entity
    Add auditing + relationship to plans

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Operator.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Operator.java
@@ -3,32 +3,39 @@ package se.lexicon.subscriptionapi.domain.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@ToString(exclude = "plans")
 @Entity
 @Table(name = "operators")
+@EntityListeners(AuditingEntityListener.class)
 public class Operator {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 100)
     @NotBlank(message = "Operator name is required")
     private String name;
 
+    @OneToMany(mappedBy = "operator", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Plan> plans = new HashSet<>();
+
+    @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
