edd37876ff9bbe94cab5641b6e9998b9e330f390
SMASobur <sikdar.se@gmail.com>
2026-06-22 10:32:53 +0200

    Add GlobalExceptionHandler exception

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,73 @@
+package se.lexicon.subscriptionapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import se.lexicon.subscriptionapi.dto.response.ErrorResponse;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ErrorResponse buildError(HttpStatus status, String message, Map<String, String> validationErrors) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                message,
+                validationErrors
+
+        );
+    }
+
+    // Handle @Valid failures
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorResponse response = buildError(HttpStatus.BAD_REQUEST, "Validation faild", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    // 404 Not Found
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+        ErrorResponse response = buildError(HttpStatus.NOT_FOUND, ex.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+    }
+
+    // 400 Bad Request (Custom business rule violation)
+    @ExceptionHandler({BusinessRuleException.class, InactivePlanException.class, PlanChangeNotAllowedException.class})
+    public ResponseEntity<ErrorResponse> handleBusinessRuleExceptions(RuntimeException ex) {
+        ErrorResponse response = buildError(HttpStatus.BAD_REQUEST, ex.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    // 401 Unauthorized (bad credentials)
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthentication(AuthenticationException ex) {
+        ErrorResponse response = buildError(HttpStatus.UNAUTHORIZED, ex.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
+    // 500 Internal Server error
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse response = buildError(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurr", null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
+    }
+}
